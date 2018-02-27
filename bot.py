04351411148e08dced4c6fa5664f515e79c8e0c4
Olga_S <Oleandora@gmail.com>
2018-02-28 00:57:14 +0300

    calculator updated

--- bot.py
+++ bot.py
@@ -20,40 +20,37 @@ def main():
     updater.idle()
 
 
+def calculate(operand1, operand2, sign):
+    try:
+        operand1, operand2 = float(operand1), float(operand2)
+        calculation = {'+': lambda x, y: x + y,
+                       '-': lambda x, y: x - y,
+                       '*': lambda x, y: x * y,
+                       '/': lambda x, y: x / y}
+        result = calculation[sign](operand1, operand2)
+        return result
+    except ValueError as e:
+        return 'Некорректный ввод, нет одного или двух чисел: {}'.format(e.args[0])
+    except ZeroDivisionError as e:
+        return 'Ай-яй-яй! Деление на ноль! Фу быть таким!: {}'.format(e.args[0])
+
+
 def talk_to_me(bot, update):
     user_text = update.message.text 
     if user_text.endswith('='):
-        oper_tuple = ('+','-', '/', '*')
+        sign_tuple = ('+', '-', '/', '*')
         evaluation = user_text
-
-        def calc(a, b, oper):
-            try:
-                a, b = float(a), float(b)
-            except ValueError:
-                update.message.reply_text('Некорректный ввод, нет одного или двух чисел')    
-            try:
-                op = {'+': (a+b), '-': (a-b),
-                      '*': (a*b), '/': (a/b)}
-                result = op[oper]
-                return result
-            except ZeroDivisionError:
-                return 'Ай-яй-яй! Деление на ноль!'
-
-            
-
-        evaluation = evaluation.strip().replace('=','')
-        f = 1
-        for operation in oper_tuple:
-            if evaluation.find(operation) != -1:
-                a, b = evaluation.split(operation)
-                update.message.reply_text(calc(a, b, operation))
-                f = 0
-                break    
-        if f:
-            update.message.reply_text('Нет математического оператора')
+        evaluation = evaluation.strip().replace('=', '')
+        for operation in sign_tuple:
+            if operation in evaluation:
+                operand1, operand2 = evaluation.split(operation)
+                update.message.reply_text(calculate(operand1, operand2, operation))
+                break
+        else:
+            update.message.reply_text('Некорректный ввод, нет математического оператора')
     else:
         update.message.reply_text(user_text)
-    
+
 
 def greet_user(bot, update):    
     update.message.reply_text('Hello!')
@@ -61,9 +58,9 @@ def greet_user(bot, update):
 
 def planet_answer(bot,update):
     planets = { "mercury": ephem.Mercury, "venus": ephem.Venus,
-            "mars": ephem.Mars, "jupiter": ephem.Jupiter, 
-            "saturn": ephem.Saturn, "uranus": ephem.Uranus,
-            "neptune": ephem.Neptune, "pluto": ephem.Pluto}
+                "mars": ephem.Mars, "jupiter": ephem.Jupiter,
+                "saturn": ephem.Saturn, "uranus": ephem.Uranus,
+                "neptune": ephem.Neptune, "pluto": ephem.Pluto}
     
     planet = update.message.text 
     planet = planet.lower().replace('/planet ','')
@@ -76,13 +73,13 @@ def planet_answer(bot,update):
 
 
 def word_count(bot,update):
-    stroka = update.message.text
-    stroka = stroka.lower().replace('/wordcount ','')
+    input_date = update.message.text
+    input_date = input_date.lower().replace('/wordcount ','')
     special_char = ('@','#','$','%','^','&','*','-','+','/','=','!','?')
 
-    if stroka:
-        if stroka.startswith('"') and stroka.endswith('"'):
-            words = stroka.replace('"','')
+    if input_date:
+        if input_date.startswith('"') and input_date.endswith('"'):
+            words = input_date.replace('"','')
             words = words.strip()
             words = words.split()
             number_of_words = len(words)
@@ -107,7 +104,4 @@ def word_count(bot,update):
     update.message.reply_text('Количество слов: {0}'.format(number_of_words))    
 
 
-
-
 main()
-
